3af30d1892f4cddf2d5cbdf070684798de0d4a2c
Lydia Easter <105994906+LydiaEaster@users.noreply.github.com>
2023-08-29 10:22:42 -0600

    Update BlinkArduinoLight.ino
    This is an edit according to the professor's preferences. It has NOT been tested.

--- BlinkArduinoLight.ino
+++ BlinkArduinoLight.ino
@@ -1,17 +1,49 @@
-/*
-*     ECE 1002  Lab 1 blink lights
-*          Lydia Easter August 2023
-*/
+//
+//                   lab_1_blinkie.ino
+//
+//    By: Lydia Easter
+//    Date: 29 August 2023
+//    Class: ECE-1002/002
+//    Assignment: Lab-1 blink lights
+//
+//    Problem Statement: Connect an LED to pin 13 and blink on-off every 1/2 second.
+//
+//             ^^^^^^  Algorithm   ^^^^^^
+// int oneSecDelay = 1000;
+// int twoSecDelay = 2000;
+//
+// set function
+//   set pin 13 as output
+//   initializee pin 13 as off
+// end setup function
+//
+// loop function
+//   turn on LED
+//   tell processor sleep one second
+//
+//   turn off LED
+//   sleep 2 sec
+// end loop function
+//
+
+const int oneSecDelay = 1000;  // global const
+const int twoSecDelay = 2000;
+const int pinNumber = 13;
+
+
 void setup() 
 {
-  pinMode(8, OUTPUT);   // initialize pin 8 as digital output to control the LED
-}
+  pinMode(pinNumber, OUTPUT);   // initialize pin pinNumber as digital output to control the LED
+  digitalWrite(pinNumber, LOW);   // initialize to OFF
+} // end setup
 
 void loop() 
 {
  // use delays and pin 8 to blink an LED on and off
- digitalWrite(8, HIGH); 
- delay(1500);
- digitalWrite(8, LOW);
- delay(1000);
-}
+ digitalWrite(pinNumber, HIGH); 
+ delay(oneSecDelay); // sleep one second
+
+ digitalWrite(pinNumber, LOW);
+ delay(twoSecDelay); // sleep two seconds
+
+} // end loop
